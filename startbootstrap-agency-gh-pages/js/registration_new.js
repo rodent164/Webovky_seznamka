9b04953fb4aedf35060cfb92db6ee89725c0cd8b
pesslovany <43780444+pesslovany@users.noreply.github.com>
2026-08-24 13:26:53 -0500

    supabase key in supabase-client.js

--- startbootstrap-agency-gh-pages/js/registration_new.js
+++ startbootstrap-agency-gh-pages/js/registration_new.js
@@ -1,11 +1,3 @@
-const SUPABASE_URL = "https://fhsptjjfxseijrironas.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoc3B0ampmeHNlaWpyaXJvbmFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU3MTg0NDQsImV4cCI6MjEwMTI5NDQ0NH0.jvEjiZQdsbVXvKT02D5ADISKli28VCmaVMpbM3h88yw";
-
-const supabaseClient = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-);
-
 supabaseClient.rpc('test_auth_role').then(({ data, error }) => {
     console.log("ROLE FROM SUPABASE:", data);
     console.log("ROLE ERROR:", error);
