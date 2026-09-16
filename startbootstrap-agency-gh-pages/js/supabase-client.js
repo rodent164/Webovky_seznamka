8c5d7972fd9b2f551155064a7e0c5521bc8f9c42
pesslovany <43780444+pesslovany@users.noreply.github.com>
2026-09-16 15:32:14 -0500

    remove startbootsrap folder

--- startbootstrap-agency-gh-pages/js/supabase-client.js
+++
@@ -1,8 +0,0 @@
-const SUPABASE_URL = "https://fhsptjjfxseijrironas.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoc3B0ampmeHNlaWpyaXJvbmFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU3MTg0NDQsImV4cCI6MjEwMTI5NDQ0NH0.jvEjiZQdsbVXvKT02D5ADISKli28VCmaVMpbM3h88yw";
-
-
-const supabaseClient = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-);
